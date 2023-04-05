642d83f3eb82b7bd4431634ec5cb581582176b62
Ryan Paglinawan <ryanpaglinawan@icloud.com>
2023-04-05 07:39:10 -0700

    Update streamlit_app.py
    added pandas library support

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -7,5 +8,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 fruit_macros = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(fruit_macros)
